8c8b55cb9c10694e1765bd0f6e25e5f499ba4351
Jon Brisbin <jon@jbrisbin.com>
2016-04-19 09:51:04 -0500

    Tweak Riak start

--- riak/overlays/riak.sh
+++ riak/overlays/riak.sh
@@ -4,8 +4,8 @@ RIAK=$RIAK_HOME/bin/riak
 RIAK_ADMIN=$RIAK_HOME/bin/riak-admin
 
 CLUSTER_NAME=${CLUSTER_NAME:-dev}
-HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[()]//g')
-MASTER=$(ping -c1 ${CLUSTER_NAME:-dev}1 | awk '/^PING/ {print $3}' | sed 's/[()]//g')||''
+HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1'
+MASTER=$(ping -c1 ${CLUSTER_NAME}1 | awk '/^PING/ {print $3}' | sed 's/[()]//g')||''
 
 sed -i -r "s#nodename = (.*)#nodename = riak@$HOST#g" /etc/riak/riak.conf
 sed -i -r "s#distributed_cookie = (.*)#distributed_cookie = $CLUSTER_NAME#g" /etc/riak/riak.conf
--- riak/riak-cluster.sh
+++ riak/riak-cluster.sh
@@ -5,11 +5,8 @@ NODES=${3:-3}
 
 case "$1" in
   start)
-    START_PORT=${START_PORT:-10017}
-    END_PORT=$(($START_PORT+(10*$NODES)))
-    PORTS=($(seq $START_PORT 10 $END_PORT))
     for i in $(seq 1 $NODES); do
-      docker run -d --name "${CLUSTER_NAME}$i" -p ${PORTS[i-1]}:8087 -e CLUSTER_NAME=$CLUSTER_NAME basho/riak-ts
+      docker run -d -h "${CLUSTER_NAME}$i" -p 8087 -p 8098 -e CLUSTER_NAME=$CLUSTER_NAME --link "${CLUSTER_NAME}1" basho/riak-ts
     done
     ;;
   stop)
