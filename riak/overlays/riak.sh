d8f84b2431a801ad0bda41820dd99598ec10e43f
Jon Brisbin <jon@jbrisbin.com>
2016-04-25 11:11:52 -0500

    tweak to cluster IP discovery

--- riak/overlays/riak.sh
+++ riak/overlays/riak.sh
@@ -5,7 +5,7 @@ RIAK_ADMIN=$RIAK_HOME/bin/riak-admin
 
 CLUSTER_NAME=${CLUSTER_NAME:-dev}
 HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1'
-MASTER=$(ping -c1 ${CLUSTER_NAME}1 | awk '/^PING/ {print $3}' | sed 's/[()]//g')||''
+MASTER=$((ping -c1 ${CLUSTER_NAME}1 2>/dev/null || echo 'PING local 127.0.0.1') | awk '/^PING/ {print $3}' | sed 's/[()]//g')||''
 
 sed -i -r "s#nodename = (.*)#nodename = riak@$HOST#g" /etc/riak/riak.conf
 sed -i -r "s#distributed_cookie = (.*)#distributed_cookie = $CLUSTER_NAME#g" /etc/riak/riak.conf
@@ -14,6 +14,7 @@ $RIAK start
 
 if [ "$HOST" != "$MASTER" ]; then
   echo "Connecting to cluster @ $MASTER"
+  $RIAK_ADMIN wait-for-service riak_kv riak@$MASTER
   $RIAK_ADMIN cluster join riak@$MASTER
   $RIAK_ADMIN cluster plan
   $RIAK_ADMIN cluster commit
