8a6327a640e89203d7b6a43cf17dfad906fa8d45
Jon Brisbin <jon@jbrisbin.com>
2016-05-27 13:29:20 -0500

    Update inventory script

--- mesos/jenkins/marathon-inventory.py
+++ mesos/jenkins/marathon-inventory.py
@@ -17,37 +17,26 @@ grp.add_argument('--list', action='store_true', help='list all hosts')
 grp.add_argument('--host', help='get meta for specific host')
 opts = p.parse_args(sys.argv[1:])
 
-marathonUrl = os.environ.get('MARATHON_URL', 'http://marathon.mesos:8080')
-regex = re.compile(os.environ.get('APP_ID', '.*'))
+mesosUrl = os.environ.get('MASTER', 'master.mesos:5050')
 group = os.environ.get('GROUP', 'mesos')
 
-data = {"_meta": {"hostvars": {}}}
+data = {}
 data[group] = []
 
 def get_tasks():
-  tasks = json.loads(urllib2.urlopen(('%s/v2/tasks' % marathonUrl)).read())
-  appInfo = json.loads(urllib2.urlopen(('%s/v2/apps' % marathonUrl)).read())
+  slaves = json.loads(urllib2.urlopen(('http://%s/slaves' % mesosUrl)).read())
 
-  for t in tasks["tasks"]:
-    if regex.match(t["appId"]):
-      host = t["host"]
-      appId = t["appId"]
-      data[group] += [host]
-      data["_meta"]["hostvars"][host] = {"appId": appId, "marathonUrl": marathonUrl}
+  for s in slaves["slaves"]:
+    data[group] += [s["hostname"]]
+
   data[group] = list(set(data[group]))
-
-  for h in data[group]:
-    for ai in appInfo["apps"]:
-      if ai["id"] == data["_meta"]["hostvars"][h]["appId"]:
-        for k in ai["env"]:
-          data["_meta"]["hostvars"][h][k] = ai["env"][k]
   return data
 
 if opts.list:
   print json.dumps(get_tasks())
 elif opts.host:
   data = get_tasks()
-  print json.dumps(data["_meta"]["hostvars"][opts.host])
+  print json.dumps({})
 else:
   p.print_help()
 
