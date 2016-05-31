80825c659e095da3367beedcdcbb7680a58d7b00
Jon Brisbin <jon@jbrisbin.com>
2016-05-31 09:37:09 -0500

    Fix bug that left off the execute() call

--- mesos/jenkins/init.groovy.d/connect-slaves.groovy
+++ mesos/jenkins/init.groovy.d/connect-slaves.groovy
@@ -8,7 +8,7 @@ Thread.start {
   String host = env['HOST']
   int port = env['PORT0'].toInteger()
 
-  for (slv in Hudson.instance.slaves){ 
-    "connect-slave.sh $host:$port ${slv.name} ${slv.getComputer().getJnlpMac()}"
+  for (slv in Hudson.instance.slaves){
+    "connect-slave.sh $host:$port ${slv.name} ${slv.getComputer().getJnlpMac()}".execute()
   }
 }
