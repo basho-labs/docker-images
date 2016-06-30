b2815b2b3305802c68d334b3372c53fa3710bd9c
Jon Brisbin <jon@jbrisbin.com>
2016-06-30 13:34:51 -0500

    Tweaks

--- riak/test/test_riak_container.py
+++ riak/test/test_riak_container.py
@@ -1,7 +1,14 @@
 import pytest
-from riak.test import cluster
+from riak import RiakClient, RiakNode
+from riak_test.test import cluster
 
 class TestRiakContainer:
 
   def test_can_start_single_node(self, cluster):
-    assert len(cluster.ips()) > 0
+
+    assert len(cluster.ips()) == 1
+
+    print ("nodes: %s" % [ RiakNode(host=ip[0], pbc_port=ip[1]) for ip in cluster.ips() ])
+
+    cl = RiakClient(nodes=[ RiakNode(host=ip[0], pbc_port=ip[1]) for ip in cluster.ips() ])
+    # assert cl.ping()
