a1ab6e08edb6a9c540ea723926bfc04a73d1e37d
Jon Brisbin <jon@jbrisbin.com>
2016-07-28 10:50:50 -0500

    Tweak test

--- tools/erlang/test/alpine.mk
+++ tools/erlang/test/alpine.mk
@@ -1,12 +1,12 @@
 TARGET_OS ?= alpine-3.4
 
-TAG = test-erlang-alpine
+TAG = test-erlang-container
 FROM = basho/erlang:$(TARGET_OS)
 OVERLAYS = greeting
 CMD = ["/tmp/greeting.sh"]
 
 test-escript: clean install
 	GREETING=`docker run --rm -i $(TAG)`; \
-	[ "hello world" == "$$GREETING" ]
+	[[ "hello world" == "$$GREETING" ]]
 
 -include ../../../docker.mk
