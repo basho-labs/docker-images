7db96b03c5c78dc72cf24b4690ad814ef974a48f
Jon Brisbin <jon@jbrisbin.com>
2016-08-31 16:57:01 -0500

    Updated tests to prepare for changes to cluster creation that make starting a cluster faster

--- riak/test/test.mk
+++ riak/test/test.mk
@@ -2,15 +2,15 @@ TAG = tests
 FROM = alpine
 MAINTAINER = Jon Brisbin <jbrisbin@basho.com>
 OVERLAYS = tests
+RIAK_MODE ?= test
 PYTEST_ARGS ?=
-CMD = $(PYTEST_ARGS)
 
 .PHONY: test-pytest-tests
 
 clean::
 	rm -Rf .cache __pycache__ riak_docker/__pycache__ riak_docker/*.pyc *.pyc
 
-test-pytest-tests: clean install
-	docker-compose -f test.yml -p $@ up
+test-pytests: clean install
+	docker-compose -f test.yml -p $@ run -e RIAK_MODE=$(RIAK_MODE) tests $(PYTEST_ARGS)
 
 include ../../docker.mk
